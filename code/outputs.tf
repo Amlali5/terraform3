2279e34539382c767fa745ce48282f7d8cc68e4c
Amlali5 <125584098+Amlali5@users.noreply.github.com>
2025-02-09 14:19:30 +0200

    Add files via upload

---
+++ code/outputs.tf
@@ -0,0 +1,4 @@
+output "public_lb_dns" {
+  value = module.load_balancer.public_lb_dns
+}
+
---
+++ code/variables.tf
@@ -0,0 +1,6 @@
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
