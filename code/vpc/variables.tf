cd542dd4b6d446fe3e467fc27fc0e4de7f29b651
Amlali5 <125584098+Amlali5@users.noreply.github.com>
2025-02-09 14:22:02 +0200

    Add files via upload

---
+++ code/vpc/variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
